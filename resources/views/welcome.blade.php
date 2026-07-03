a002c5b6cb3268e7f21f13e4aeda98db38a7c735
rinigwulandari09 <rini23ti@mahasiswa.pcr.ac.id>
2026-07-04 00:02:58 +0700

    tambah pengingat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -269,6 +269,9 @@
                 </div>
             </main>
         </div>
+        <div>
+            
+        </div>
 
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
