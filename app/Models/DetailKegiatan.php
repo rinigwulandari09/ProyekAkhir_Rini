4b718f1119a432e83a8398da3145400376fe6280
rinigwulandari09 <rini23ti@mahasiswa.pcr.ac.id>
2026-07-06 13:59:56 +0700

    perbaiki api kegiatan, dan tambah model DetailKegiatan

--- app/Models/DetailKegiatan.php
+++ app/Models/DetailKegiatan.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class JenisKegiatan extends Model
+class DetailKegiatan extends Model
 {
     protected $table = 'detail_kegiatan';
 
