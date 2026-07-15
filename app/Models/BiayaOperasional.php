a5295b5dfa02aa9bb8e04c7e62b923d216d1b456
nitayeee <nita23ti@mahasiswa.pcr.ac.id>
2026-07-15 14:30:30 +0700

    Perbaiki nama atribut 2

--- app/Models/BiayaOperasional.php
+++ app/Models/BiayaOperasional.php
@@ -7,7 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class BiayaOperasional extends Model
 {
     protected $table = 'biaya_operasional';
+    protected $primaryKey = 'id'; 
 
+    // 2. JIKA tipe data 'id' di PostgreSQL Anda menggunakan tipe SERIAL / BIGSERIAL (Integer otomatis), set ke true.
+    //    JIKA tipe data 'id' adalah UUID / String, ubah menjadi false.
+    public $incrementing = true; 
+
+    // 3. Sesuaikan tipe datanya ('int' jika integer, 'string' jika UUID/Text)
+    protected $keyType = 'int';
+    
     protected $fillable = [
         'biaya_tanggal',
         'biaya_nama',
